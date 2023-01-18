73b5e50c57f49015ac24558a58772643c9257a1f
DariaSem <sdasha9328@gmail.com>
2023-01-18 20:34:10 +0300

    fibonacci_v3

--- fibonacci/fibonacci.py
+++ fibonacci/fibonacci.py
@@ -2,9 +2,8 @@ def fibonacci_function(to_step):
     f1 = f2 = 1
     count = 2
     while count < to_step:
-        fsum = f1 + f2
-        f1 = f2
-        f2 = fsum
+        f2 = f1 + f2
+        f1 = f2 -f1
         count += 1
     return f2
 
