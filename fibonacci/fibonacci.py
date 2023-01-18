14d026a0a88dd8a0999a465cafbea37b9f719943
DariaSem <sdasha9328@gmail.com>
2023-01-18 19:30:17 +0300

    fibonacci_v2

--- fibonacci/fibonacci.py
+++ fibonacci/fibonacci.py
@@ -9,4 +9,13 @@ def fibonacci_function(to_step):
     return f2
 
 
+def fibonacci_sequence(to_step):
+    sequence = [1, 1]
+    if to_step == 1:
+        return 1
+    else:
+        for i in range(to_step - 2):
+            sequence.append(sequence[i] + sequence[i + 1])
+    return sequence
 
+print(fibonacci_sequence(7))
--- fibonacci/test_fibonacci.py
+++ fibonacci/test_fibonacci.py
@@ -4,13 +4,19 @@ from fibonacci import fibonacci_function, fibonacci_sequence
 
 class FibonacciTest(unittest.TestCase):
     def test_fibonacci_to_1(self):
-        result = fibonacci_function(1)
-        self.assertEqual(1, result)
+        self.assertEqual(1, fibonacci_function(1))
 
     def test_fibonacci_to_10(self):
-        result = fibonacci_function(10)
-        self.assertEqual(55, result)
+        self.assertEqual(55, fibonacci_function(10))
 
     def test_fibonacci_to_100(self):
-        result = fibonacci_function(100)
-        self.assertEqual(354224848179261915075, result)
+        self.assertEqual(354224848179261915075, fibonacci_function(100))
+
+
+class FibonacciSequenceTest(unittest.TestCase):
+    def test_sequence_to_1(self):
+        self.assertEqual(1, fibonacci_sequence(1))
+
+    def test_sequence_to_7(self):
+        self.assertEqual([1, 1, 2, 3, 5, 8, 13], fibonacci_sequence(7))
+        
