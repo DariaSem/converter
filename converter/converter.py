09e58239224446c5a0b40b378e4d6ac637ba2a4a
DariaSem <sdasha9328@gmail.com>
2023-01-27 13:01:37 +0300

    converter_v4

--- converter/converter.py
+++ converter/converter.py
@@ -2,20 +2,17 @@ from abc import ABC, abstractmethod
 
 
 class Converter(ABC):
-    def __init__(self, string):
-        self.string = string
-
-    def get_digit_list(self):
-        return [ord(i) for i in self.string]
+    def get_digit_list(self, string):
+        return [ord(i) for i in string]
 
     @abstractmethod
-    def convert(self):
+    def convert(self, string):
         pass
 
 
 class BinaryConverter(Converter):
-    def convert(self):
-        digit_list = self.get_digit_list()
+    def convert(self, string):
+        digit_list = self.get_digit_list(string)
         bin_digit = []
         for digit in digit_list:
             symbol = ''
@@ -29,32 +26,26 @@ class BinaryConverter(Converter):
 
 
 class OctalConverter(Converter):
-    def convert(self):
-        digit_list = self.get_digit_list()
+    def convert(self, string):
+        digit_list = self.get_digit_list(string)
         return [oct(i).replace('0o', '') for i in digit_list]
 
 
 class HexadecimalConverter(Converter):
-    def convert(self):
-        digit_list = self.get_digit_list()
+    def convert(self, string):
+        digit_list = self.get_digit_list(string)
         return [hex(i).replace('0x', '') for i in digit_list]
 
 
-class Notation:
-    def __init__(self, string, notation):
-        self.string = string
-        self.notation = notation
-
-    def choose_converter(self):
-        if self.notation == 2:
-            return BinaryConverter(self.string).convert()
-        elif self.notation == 8:
-            return OctalConverter(self.string).convert()
-        elif self.notation == 16:
-            return HexadecimalConverter(self.string).convert()
+class ConverterFactory:
+    def get_converter(self, notation):
+        if notation == 2:
+            return BinaryConverter()
+        elif notation == 8:
+            return OctalConverter()
+        elif notation == 16:
+            return HexadecimalConverter()
 
 
-def converter(string, notation):
-    return Notation(string, notation).choose_converter()
-
-
+converterFactory = ConverterFactory()
+print(converterFactory.get_converter(2).convert('hgjgj'))
